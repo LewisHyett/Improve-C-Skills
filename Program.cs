211646615ad0d3db0f59cb410ae693d39b5084c7
LewisHyett <44363134+LewisHyett@users.noreply.github.com>
2020-06-23 22:11:14 +0100

    Added extra functionalouty to the JSON API

--- Program.cs
+++ Program.cs
@@ -2,31 +2,55 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Improve_C_Skills
 {
     class Program
     {
 
-        private const string URL = "https://jsonplaceholder.typicode.com/posts/";
+        private const string URL = "https://jsonplaceholder.typicode.com/posts/1";
         static void Main(string[] args)
-        {
+        {            
 
-            System.Console.WriteLine("Test");
-            
             using (HttpClient client = new HttpClient())
             {
+                string responseString;
+                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri(URL)))
+                {
+                    HttpResponseMessage response = client.SendAsync(request).Result;
 
-                client.BaseAddress = new Uri(URL);
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    // Get the response content as a string
+                    responseString = response.Content.ReadAsStringAsync().Result;                    
+                    var o = JObject.Parse(responseString);
 
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get,"");
-                request.Content = new StringContent(URL, Encoding.UTF8,"application/json");
+                    //Loop throgh all Values
+                    foreach (var item in o)
+                    {
+                        System.Console.WriteLine(item.Value.ToString());
+                    }
+                    JToken value;
+                    //Get a specific value
+                    if (o.TryGetValue("userId", out value))
+                    {
+                        System.Console.WriteLine(value.ToString());
+                    }
 
-               
+                    // Add a new Key>value
+                    o.Add("Developer", "Lewis");
+                    //Display new JSON Object
+                    foreach (var item in o)
+                    {
+                        System.Console.WriteLine(item.Value.ToString());
+                    }
+                    
+                }
 
 
             }
+
+            
         }
     }
 }
